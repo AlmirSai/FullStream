3f3b0368438d8c6c8b859c91d83d1e993daa1c08
Almir <167138187+AlmirSai@users.noreply.github.com>
2025-01-02 14:45:52 +0300

    Initial commmit: Add fundamental for the future updates

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import RedisStore from 'connect-redis'
+import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
+
+import { ms, type StringValue } from '@/src/shared/utils/ms.util'
+import { parseBoolearn } from '@/src/shared/utils/parse-boolearn.util'
+
+import { CoreModule } from './core/core.module'
+import { RedisService } from './core/redis/redis.service'
+
+async function bootstrap() {
+	const app = await NestFactory.create(CoreModule)
+
+	const config = app.get(ConfigService)
+	const redis = app.get(RedisService)
+
+	app.use(cookieParser(config.getOrThrow<string>('COOKIE_SECRET')))
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true
+		})
+	)
+
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+				httpOnly: parseBoolearn(
+					config.getOrThrow<string>('SESSION_HTTP_ONLY')
+				),
+				secure: parseBoolearn(
+					config.getOrThrow<string>('SESSION_SECURE')
+				),
+				sameSite: 'lax'
+			},
+			store: new RedisStore({
+				client: redis,
+				prefix: config.getOrThrow<string>('SESSION_FOLDER')
+			}),
+		})
+	)
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+		credentials: true,
+		exposedHeaders: ['set-cookie']
+	})
+
+	await app.listen(config.getOrThrow<number>('APPLICATION_PORT'))
+}
+bootstrap()
